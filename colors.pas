8bc5406ba60c46015290e933a7399937cb7ee149
Kapibarki <127871025+Kapibarki@users.noreply.github.com>
2023-03-16 19:41:15 +0300

    Add files via upload

---
+++ colors.pas
@@ -0,0 +1,20 @@
+program colortest;
+uses Crt;
+begin
+TextColor(Green);
+writeln('GREEN');
+TextColor(Blue);
+writeln('BLUE');
+TextColor(Yellow);
+writeln('YELLOW');
+TextColor(Red);
+writeln('RED');
+TextColor(Cyan);
+writeln('CYAN');
+TextColor(Brown);
+writeln('BROWN');
+TextColor(White);
+writeln('WHITE');
+writeln('KapyColors 1.0');
+readln;
+end.
